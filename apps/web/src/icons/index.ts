336fc94580df3276e6704bbb8a8c0fdfd008d44b
Saatvik Arya <aryasaatvik@gmail.com>
2026-02-09 13:22:03 +0530

    feat: add GitHub icon component and export in index

--- apps/web/src/icons/index.ts
+++ apps/web/src/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as Download3 } from "./download";
+export { default as GitHub } from "./github";
 export { default as Mcp } from "./mcp";
 export { default as Plug } from "./plug";
 export { default as Server } from "./server";
